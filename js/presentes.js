ce3adafbddbacb458c69f9e41002cf85e6a9b663
Felipe Marchi <fel.marchi@gmail.com>
2025-01-19 13:41:08 -0300

    First commit

---
+++ js/presentes.js
@@ -0,0 +1,248 @@
+
+const data = {
+    "presentes": [
+        {
+            "descricao": "Jogo de Assadeiras Antiaderente Retangular",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_2X_858550-MLB74672526076_022024-F.webp",
+            "preco": "277,00",
+            "link": "https://produto.mercadolivre.com.br/MLB-4477557942-jogo-4-assadeiras-forma-antiaderente-retangular-assado-bolo-_JM?matt_tool=92309335&matt_word=&matt_source=bing&matt_campaign=MLB_ML_BING_AO_HOME%20%26%20INDUSTRY-ALL-ALL_X_PLA_ALLB_TXS_ALL&matt_campaign_id=382858298&matt_ad_group=HOME%20%26%20INDUSTRY&matt_match_type=e&matt_network=o&matt_device=c&matt_keyword=default&msclkid=82d6f6343e2f1bc1180691bc2ce276b1&utm_source=bing&utm_medium=cpc&utm_campaign=MLB_ML_BING_AO_HOME%20%26%20INDUSTRY-ALL-ALL_X_PLA_ALLB_TXS_ALL&utm_term=4581252654962817&utm_content=HOME%20%26%20INDUSTRY"
+        },
+        {
+            "descricao": "Ajude a Vitória a lavar louça",
+            "imagem": "https://fastshopbr.vtexassets.com/arquivos/ids/194592/0-BRBLF08BSANA-PRD-1500-1.jpg?v=638701422900700000",
+            "preco": "2.210,00",
+            "link": "https://site.fastshop.com.br/lava-loucas-brastemp-cinza-metalico-com-08-servicos-e-05-programas-de-lavagem---blf08b-brblf08bsana_prd/p?msclkid=5afe107d6c641262ca0277f56325981c"
+        },
+        {
+            "descricao": "A Vitória ronca",
+            "imagem": "https://http2.mlstatic.com/earmuff-felpudo-preto-inverno-protetor-aquecedor-de-orelhas-D_NQ_NP_15781-MLB20108232939_062014-F.jpg",
+            "preco": "35,00",
+            "link": ""
+        },
+        {
+            "descricao": "Adega de Vinhos Climatizada",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/adega-de-vinhos-climatizada-brastemp-para-12-garrafas-bzc12be-127v/zupostore2/1810100057/0263b7a56fba0775f5c4894091daa3ac.jpeg",
+            "preco": "1.199,00",
+            "link": "https://www.magazineluiza.com.br/adega-de-vinhos-climatizada-brastemp-para-12-garrafas-bzc12be-127v/p/kd543635hk/ed/adeg/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_ele_sor_3p_ed-std&partner_id=76174&utm_term=76174&gclid=9e503bdedf68103028725c56d18bae34&gclsrc=3p.ds&msclkid=9e503bdedf68103028725c56d18bae34"
+        },
+        {
+            "descricao": "Lua De Mel",
+            "imagem": "https://virtualjoias.com/media/wysiwyg/virtualjoias-0902-Lugares-para-passar-lua-de-mel-imagem5-blog_1.jpg",
+            "preco": "1.500,00",
+            "link": ""
+        },
+        {
+            "descricao": "Aposentadoria dos noivos",
+            "imagem": "https://classeturista.com/wp-content/uploads/2018/08/Idosos-em-Viagem.jpg",
+            "preco": "1.000,00",
+            "link": ""
+        },
+        {
+            "descricao": "Aparelho De Jantar e chá",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/aparelho-de-jantar-e-cha-30-pecas-ryo-maresia-oxford/olistplus/opmxebpp7v0zve84/a84a3e71ac336f2b3722ee6057ad7a69.jpeg",
+            "preco": "598,11",
+            "link": "https://www.magazineluiza.com.br/aparelho-de-jantar-e-cha-30-pecas-ryo-maresia-oxford/p/fafkj04034/ud/apja/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_3p_all-products-std&partner_id=77025&utm_term=77025&msclkid=8d7f477233c11c90362620fa23f77b85&gclid=8d7f477233c11c90362620fa23f77b85&gclsrc=3p.ds"
+        },
+        {
+            "descricao": "Robô Aspirador e Passa Pano",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/robo-aspirador-e-passa-pano-kabum-smart-900-220v-preto-kbsf010/kabum/366170/9182db7e91e94d086d5d7907c6d52a94.jpeg",
+            "preco": "1.569,00",
+            "link": "https://www.magazineluiza.com.br/robo-aspirador-e-passa-pano-kabum-smart-900-220v-preto-kbsf010/p/hbaf4k7bja/ep/eprb/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_3p_all-products-std&partner_id=77025&utm_term=77025&gclid=e8b60afc3d4612ca7c346d742d87152a&gclsrc=3p.ds&msclkid=e8b60afc3d4612ca7c346d742d87152a"
+        },
+        {
+            "descricao": "Aulas de dança para o Felipe",
+            "imagem": "https://thumbs.dreamstime.com/b/dan%C3%A7arino-de-hip-hop-5401776.jpg",
+            "preco": "99,90",
+            "link": ""
+        },
+        {
+            "descricao": "Bolinhos da Vivi",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/batedeira-planetaria-oster-4l-850w-preto-inox-127v-obat641-127/continentalcenter/280212501030210011/61f88cacd0c3dfee08ffbb0181a8e382.jpeg",
+            "preco": "475,00",
+            "link": "https://www.magazineluiza.com.br/batedeira-planetaria-oster-4l-850w-preto-inox-127v-obat641-127/p/hbd10bc559/ep/btdc/"
+        },
+        {
+            "descricao": "Casa cheirosinha",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_2X_946973-MLB79798374503_102024-F.webp",
+            "preco": "65,00",
+            "link": "https://produto.mercadolivre.com.br/MLB-3025907594-difusor-aromatizador-de-ambiente-eletrico-aromaterapia-oleos-_JM?matt_tool=80367574&matt_word=&matt_source=bing&matt_campaign=MLB_ML_BING_AO_BEAUTY%20%26%20HEALTH-ALL-ALL_X_PLA_ALLB_TXS_ALL&matt_campaign_id=382858294&matt_ad_group=BEAUTY%20%26%20HEALTH&matt_match_type=e&matt_network=o&matt_device=c&matt_keyword=default&msclkid=c3656df977fd15104e820a716fd37124&utm_source=bing&utm_medium=cpc&utm_campaign=MLB_ML_BING_AO_BEAUTY%20%26%20HEALTH-ALL-ALL_X_PLA_ALLB_TXS_ALL&utm_term=4582008568496629&utm_content=BEAUTY%20%26%20HEALTH"
+        },
+        {
+            "descricao": "Faqueiro Tramontina",
+            "imagem": "https://assets.tramontina.com.br/upload/tramon/imagens/CUT/23799071PDM001G.jpg",
+            "preco": "75,00",
+            "link": "https://www.tramontina.com.br/faqueiro-tramontina-buzios-cem-aco-inox-com-detalhe-24-pecas/23799071.html"
+        },
+        {
+            "descricao": "Churrascão do Felipão",
+            "imagem": "https://imgs.ponto.com.br/1521377419/1xg.jpg",
+            "preco": "144,12",
+            "link": "https://www.pontofrio.com.br/faqueiro-para-churrasco-inox-tramontina-jumbo-12-pecas/p/1521377419?utm_campaign=DescontoEspecial&utm_medium=BuscaOrganica&utm_source=Bing"
+        },
+        {
+            "descricao": "Sanduicheira Elétrica",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/sanduicheira-eletrica-click-preta-cadence-220v/xstore/san400220/a672b16550bf9ea57a577bfeeee5ed0c.jpeg",
+            "preco": "123,11",
+            "link": "https://www.magazineluiza.com.br/sanduicheira-eletrica-click-preta-cadence-220v/p/eh40f692bh/ep/gset/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_3p_all-products-std&partner_id=77025&utm_term=77025&msclkid=c9d55397a202154d66fe341fb81c0ae3&gclid=c9d55397a202154d66fe341fb81c0ae3&gclsrc=3p.ds"
+        },
+        {
+            "descricao": "Máquina De Waffle",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_2X_669723-MLB47708121959_092021-F.webp",
+            "preco": "195,41",
+            "link": "https://produto.mercadolivre.com.br/MLB-1742201071-maquina-de-waffle-grill-pratic-mondial-1200w-gw-01-_JM?matt_tool=14213447&matt_word=&matt_source=bing&matt_campaign=MLB_ML_BING_AO_CE-ALL-ALL_X_PLA_ALLB_TXS_ALL&matt_campaign_id=382858295&matt_ad_group=CE&matt_match_type=e&matt_network=o&matt_device=c&matt_keyword=default&msclkid=7733f9d523111a58c1fa9e5d39631f45&utm_source=bing&utm_medium=cpc&utm_campaign=MLB_ML_BING_AO_CE-ALL-ALL_X_PLA_ALLB_TXS_ALL&utm_term=4581596253419776&utm_content=CE"
+        },
+        {
+            "descricao": "Primeiro jantar dos noivos",
+            "imagem": "https://th.bing.com/th?id=OPHS.wdag36BuXiTfTg474C474&w=248&h=248&c=17&o=5&dpr=1.3&pid=21.1",
+            "preco": "59,70",
+            "link": ""
+        },
+        {
+            "descricao": "Jogo de Taças para Vinho",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/jogo-de-tacas-para-vinho-de-cristal-460ml-6-pecas-haus-concept-pleasure/magazineluiza/237989200/41026b029a221d86e378b142515227b0.jpg",
+            "preco": "145,40",
+            "link": "https://www.magazineluiza.com.br/jogo-de-tacas-para-vinho-de-cristal-460ml-6-pecas-haus-concept-pleasure/p/237989200/ud/tavi/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_1p_all-products-std&partner_id=65140&utm_term=65140&gclid=ce7503777cee10fe433564ff67d714b7&gclsrc=3p.ds&msclkid=ce7503777cee10fe433564ff67d714b7"
+        },
+        {
+            "descricao": "Liquidificador De Vidro",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_2X_992756-MLB26570764641_122017-F.webp",
+            "preco": "336,00",
+            "link": "https://produto.mercadolivre.com.br/MLB-954816042-liquidificador-oster-osterizer-600w-jarra-de-vidro-vermelho-_JM"
+        },
+        {
+            "descricao": "Mop 360 Triangular",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/mop-360-triangular-com-2-refil-limpeza-laves/laves01/esfre-trian-58/2ac4c4bdcef2c1cc8a8a4c443b3bbb3f.jpeg",
+            "preco": "38,74",
+            "link": "https://www.magazineluiza.com.br/mop-360-triangular-com-2-refil-limpeza-laves/p/efa357d97f/ud/mopi/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_3p_all-products-std&partner_id=77025&utm_term=77025&msclkid=85c1afc6475e1dc439e3c84e5bdb5533&gclid=85c1afc6475e1dc439e3c84e5bdb5533&gclsrc=3p.ds"
+        },
+        {
+            "descricao": "Kit Potes Herméticos",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/kit-potes-hermeticos-plasticos-cinza-8-unidades-electrolux/eletromiller/41046083/fee489fc936922b4ef5c156bd6fa3f1a.jpeg",
+            "preco": "109,99",
+            "link": "https://www.magazineluiza.com.br/kit-potes-hermeticos-plasticos-cinza-8-unidades-electrolux/p/dhe254d7a2/ud/poud/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_3p_all-products-std&partner_id=77025&utm_term=77025&msclkid=9aeff425a9c519ed253193adb4a82ab2&gclid=9aeff425a9c519ed253193adb4a82ab2&gclsrc=3p.ds"
+        },
+        {
+            "descricao": "Panela Wok Antiaderente",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/panela-wok-tramontina-loreto-antiaderente-max-vermelho-36-l-28-cm/homepoint/se7004/c18804c789cd919045824bfc6127033c.jpeg",
+            "preco": "56,99",
+            "link": "https://www.magazineluiza.com.br/panela-wok-tramontina-loreto-antiaderente-max-vermelho-36-l-28-cm/p/bgh14ea3d3/ud/pawo/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_all_sor_3p_all-products-std&partner_id=77025&utm_term=77025&msclkid=181994624aeb125bbc3df3ea5754a055&gclid=181994624aeb125bbc3df3ea5754a055&gclsrc=3p.ds"
+        },
+        {
+            "descricao": "Risotos da Vivi",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/panela-wok-antiaderente-tramontina-paris-de-aluminio-chumbo-36cm/magazineluiza/234985900/cff97fce476be8847a7d9e635b4c51e3.jpg",
+            "preco": "349,20",
+            "link": "https://www.magazineluiza.com.br/panela-wok-antiaderente-tramontina-paris-de-aluminio-chumbo-36cm/p/234985900/ud/pawo/"
+        },
+        {
+            "descricao": "Soninho da Vivi",
+            "imagem": "https://m.media-amazon.com/images/I/8181AcXd5WL._AC_SL1500_.jpg",
+            "preco": "200,00",
+            "link": ""
+        },
+        {
+            "descricao": "Desestresse a Vivi",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_2X_732813-MLB81648426403_122024-F.webp",
+            "preco": "156,00",
+            "link": ""
+        },
+        {
+            "descricao": "Só para não dizerem que não dei nada",
+            "imagem": "https://trecobox.com.br/wp-content/uploads/2021/07/Julius-edit.jpg",
+            "preco": "112,00",
+            "link": ""
+        },
+        {
+            "descricao": "Impressora Brother",
+            "imagem": "https://a-static.mlcdn.com.br/800x560/impressora-brother-hl1212w-laser-monocromatico-wi-fi-wireless-preto-e-110v/kennedystore/569572/a55eaa41ad9bb127da8fa4ecf75777be.jpeg",
+            "preco": "1.900,00",
+            "link": "https://www.magazineluiza.com.br/impressora-brother-hl1212w-laser-monocromatico-wi-fi-wireless-preto-e-110v/p/bk9kbg2ee1/in/ipst/?utm_source=bing&utm_medium=cpc&utm_campaign=bing_eco_per_ven_sho_tc_sor_3p_in-std&partner_id=76176&utm_term=76176&msclkid=e7f3889233c819275623ef1a13ade832&gclid=e7f3889233c819275623ef1a13ade832&gclsrc=3p.ds"
+        },
+        {
+            "descricao": "Videogame da cozinha",
+            "imagem": "https://m.media-amazon.com/images/I/510kKaOeNmL._AC_SX679_.jpg",
+            "preco": "2.999,00",
+            "link": "https://www.amazon.com.br/novo-echo-show-15-com-alexa/dp/B0C7XPTW5K/ref=sr_1_1?dib=eyJ2IjoiMSJ9.R42Q_XO8DlTsfXdH8bQk3t_24_dYxGRdlRF8AjmRaMEv2xL9hPcxr_VLty_evKBtoPQ0rYAazc-3wiHxNARqyI6Szprh1REAWuye2NhZqNRhmmiOwKgl_GKObu1FCCqdjEkjtDjEEcaZCJ43J8aLbUZ0WQ9ouMQGkuQB27jmFjryIFMBsdkcBcpBhy2Glo1SSj2f1GaSGJgJfu9w1ZGszxTbSGreiCQANfMB95Cxd55HzwpP1YcA_ORWGZRQDwQoXLtQvqif8W8LeOHrHeidA7IgOufP4kaZOvUvHEllYUn-iY3-JQArdctAlNHAjwgnkHZKuCFFRhp-W4xSR-maD4IBcsr9WPOlFlkmn_qQt2AEXi7X7Mcv9MYH2DVlsRqoGFSoHjr9qXFtPlkSUkv_QaHAprrmsl5A_yOCFbAqj5XkqjlRb8ya2T5rfEsytDGE.16V1ALqtNhinvI3fYKrdQsc13QDudbQZblo9bjLzw4M&dib_tag=se&keywords=alexa+15&qid=1736977533&sr=8-1&ufe=app_do%3Aamzn1.fos.95de73c3-5dda-43a7-bd1f-63af03b14751"
+        },
+        {
+            "descricao": "Para as visitas",
+            "imagem": "https://d30xhje6ivbaf5.cloudfront.net/Custom/Content/Products/42/81/4281_jg-simples-sofee-kacyumara-bily-pr-18407-5253-jgs_z2_638477612642316041.webp",
+            "preco": "119,90",
+            "link": "https://www.lojaskacyumara.com.br/jg-simples-sofee-kacyumara-bily-pr-18407-5253-jgs-p4281"
+        },
+        {
+            "descricao": "Repouso dos guerrero",
+            "imagem": "https://http2.mlstatic.com/D_NQ_NP_690102-MLB75025742777_032024-O.webp",
+            "preco": "213,90",
+            "link": "https://produto.mercadolivre.com.br/MLB-3622120053-boia-rede-inflavel-reclinavel-grande-duas-pessoas-2-unidades-_JM#polycard_client=search-nordic&position=14&search_layout=grid&type=item&tracking_id=5e9795a0-1c8b-4c11-b1a6-415926fdd5f8"
+        },
+        {
+            "descricao": "Spray de ambiente",
+            "imagem": "https://www.granado.com.br/media/catalog/product/e/a/ean7896512955388_52852.jpg?optimize=high&fit=bounds&height=1614&width=1254&canvas=1254:1614",
+            "preco": "115,00",
+            "link": "https://www.granado.com.br/granado/spray-de-ambiente-phebo-lavanda-150ml"
+        },
+        {
+            "descricao": "Sonzão pros filmes",
+            "imagem": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT70TZqjegsisgNeN0zq4nCvGFbXDjbM1RCTMMiEIrtF9_Wyjmr5TXi7tv7RMbNQZiwl7KwYcfpQYfCi24NUrSVt77_C4GPiotTdbRWbeXSo5GUNTgM8zz5&usqp=CAE",
+            "preco": "683,61",
+            "link": "https://pt.aliexpress.com/item/1005007962448499.html?src=google&pdp_npi=4%40dis!BRL!683.61!683.61!!!!!%40!12000043049593505!ppc!!!"
+        },
+        {
+            "descricao": "Tapetão pra sala nova",
+            "imagem": "https://down-br.img.susercontent.com/file/br-11134207-7r98o-m130wc0x3jfi42.webp",
+            "preco": "274,90",
+            "link": "https://shopee.com.br/product/418336777/23297903206?gads_t_sig=VTJGc2RHVmtYMTlxTFVSVVRrdENkVHQ3ZkZSUTMrR3pBWmZZNzdrcnRBMHJEQWNOQkhRQXBGUjhJK1EyNldxd1RLVXNSR1BDUGYwcDU5SWYyOFc0bENjcnZydy94K05YVHJxa2xrRlVVZUZhTUpZZmxsZDFSNmJsK2V3cHRpQWMwT3dDR1l0a3RTRG1PQTBQc20xRktRPT0"
+        },
+        {
+            "descricao": "Vale Tudo",
+            "imagem": "https://incentivale.com.br/images/portfolio/portf_havan.jpg",
+            "preco": "300,00",
+            "link": "https://www.havan.com.br/cartao-presente-havan"
+        }
+    ]
+};
+
+// Referência ao UL
+const galleryList = document.getElementById("fh5co-gallery-list");
+let cont = 1;
+
+data.presentes.sort((a, b) => {
+    // Remover o caractere ',' e converter para número
+    const precoA = parseFloat(a.preco.replace('.', '').replace(',', '.'));
+    const precoB = parseFloat(b.preco.replace('.', '').replace(',', '.'));
+    return precoA - precoB; // Ordenação crescente
+});
+
+// Função para criar as LIs dinamicamente
+data.presentes.forEach(presente => {
+    // Cria o elemento <li>
+    const li = document.createElement("li");
+    li.className = "one-third animate-box";
+    li.setAttribute("data-animate-effect", "fadeIn");
+    li.style.backgroundImage = `url(${presente.imagem})`;
+
+    const icon = presente.link !== ''
+        ? '<span class="icon" style="color:white;font-size:30px;"><i class="fa-solid fa-arrow-up-right-from-square"></i></span>'
+        : '<span class="icon" style="color:white;font-size:30px;"><i class="fa-brands fa-pix"></i></span>';
+
+    const link = presente.link !== ''
+        ? `<a href="${presente.link}" target="_blank">`
+        : `<a href="./images/vf/qrcode.png" data-fancybox data-caption="Clique fora para voltar">`;
+        
+
+    // Conteúdo interno do <li>
+    li.innerHTML = `
+    ${link}
+      <div class="case-studies-summary">
+        <span>R$ ${presente.preco}</span>
+        <h2>${presente.descricao}</h2>
+      </div>
+      <div class="case-studies-icon">
+        ${icon}
+      </div>
+    </a>
+  `;
+
+    // Adiciona o <li> ao <ul>
+    galleryList.appendChild(li);
+});
